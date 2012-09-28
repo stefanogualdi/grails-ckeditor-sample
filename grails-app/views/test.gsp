faabf332a7c43bf89e46482f1d0d64250e278588
Stefano Gualdi <stefano.gualdi@gmail.com>
2012-09-28 16:03:36 +0200

    Initial commit

---
+++ grails-app/views/test.gsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<ckeditor:resources/>
+</head>
+<body>
+<ckeditor:editor name="test" toolbar="custom"></ckeditor:editor>
+</body>
+</html>
---
+++ web-app/js/ckeditor/ckconfig.js
@@ -0,0 +1,17 @@
+
+CKEDITOR.plugins.addExternal('MediaEmbed', '/ckplug/js/ckeditor/plugins/mediaembed/');
+
+CKEDITOR.editorConfig = function( config )
+{
+	config.extraPlugins = 'MediaEmbed';
+
+    config.toolbar_custom = [
+        ['Templates', 'Styles','Format','Font','FontSize','TextColor','BGColor','Maximize','Image'],
+        ['Source'],
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript','-','SpellChecker', 'Scayt'],
+        ['Table','HorizontalRule'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','Link','Unlink','Anchor','Smiley','SpecialChar'],
+		['MediaEmbed']
+    ]
+};
